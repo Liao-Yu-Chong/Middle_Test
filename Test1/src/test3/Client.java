9774d6fc423efbfac090ec59faf29a5c545457d7
Chong <97482664+Liao-Yu-Chong@users.noreply.github.com>
2022-10-22 16:54:44 +0800

    feat: 輸出 系統飲料店結果

--- Test1/src/test3/Client.java
+++ Test1/src/test3/Client.java
@@ -10,15 +10,20 @@ package test3;
  */
 public class Client {
 
-    public void test() {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
         DrinkShop drinkShop = new DrinkShop();
 
         //點一杯烏龍茶
-        Tea tea = (Tea) drinkShop.order("Tea", Drink.SugarType.FREE, Drink.IceType.EASY);
+        Tea tea = (Tea) drinkShop.order("tea", Drink.SugarType.FREE, Drink.IceType.EASY);
         tea.setTeaType(Tea.TeaType.OOLONG);
+        System.out.println(tea.getIce() + " " + tea.getSugar() + " " + tea.getTeaType());
 
         //點一杯美式
         Coffee coffee = (Coffee) drinkShop.order("coffee", Drink.SugarType.QUARTER, Drink.IceType.HOT);
         coffee.setCoffeeType(Coffee.CoffeeType.AMERICANO);
+        System.out.println(coffee.getIce() + " " + coffee.getSugar() + " " + coffee.getCoffeeType());
     }
 }
--- Test1/src/test3/DrinkShop.java
+++ Test1/src/test3/DrinkShop.java
@@ -20,5 +20,6 @@ public class DrinkShop {
             default ->
                 null;
         };
+
     }
 }
